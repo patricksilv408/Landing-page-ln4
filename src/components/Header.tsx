017a083380828f51edd2c1f50204314ef7666f88
[dyad] <patrick.santosilv@gmail.com>
2025-10-11 23:34:22 -0300

    [dyad] Updating website logo - wrote 3 file(s), deleted 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export const Header = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <a href="#" className="flex-shrink-0">
-            <img className="h-10 w-auto" src="/images/logo-white.jpg" alt="Alphaville Litoral Norte 4" />
+            <img className="h-10 w-auto" src="/images/logo-white.png" alt="Alphaville Litoral Norte 4" />
           </a>
           <nav className="hidden md:flex items-center space-x-8">
             {NAV_LINKS.map((link) => (
